e79824a502ac05a65dc9f16c679fd389728b8168
Oliver Walters <oliver.henry.walters@gmail.com>
2025-06-25 03:10:13 +0000

    Implement chart

--- frontend/src/StockHistoryPanel.tsx
+++ frontend/src/StockHistoryPanel.tsx
@@ -4,11 +4,128 @@ import {
   type InvenTreePluginContext,
   ModelType
 } from '@inventreedb/ui';
-import { Group, Stack } from '@mantine/core';
+import { type ChartTooltipProps, LineChart } from '@mantine/charts';
+import { Divider, Group, Paper, Stack, Text } from '@mantine/core';
 import { type DateValue, MonthPickerInput } from '@mantine/dates';
 import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
 import { useEffect, useMemo, useState } from 'react';
+
+function StockHistoryTooltip({ label, payload }: Readonly<ChartTooltipProps>) {
+  const formattedLabel: string = useMemo(() => {
+    if (label && typeof label === 'number') {
+      return dayjs(label).format('YYYY-MM-DD') ?? label;
+    } else if (!!label) {
+      return label.toString();
+    } else {
+      return '';
+    }
+  }, [label]);
+
+  if (!payload) {
+    return null;
+  }
+
+  const quantity = payload.find((item) => item.name == 'quantity');
+  const value_min = payload.find((item) => item.name == 'value_min');
+  const value_max = payload.find((item) => item.name == 'value_max');
+
+  // TODO: Better formatting of "price range"
+
+  return (
+    <Paper px='md' py='sm' withBorder shadow='md' radius='md'>
+      <Text key='title'>{formattedLabel}</Text>
+      <Divider />
+      <Text key='quantity' fz='sm'>
+        {`Quantity`} : {quantity?.value}
+      </Text>
+      <Text key='values' fz='sm'>
+        {`Value`} : {value_min?.value} - {value_max?.value}
+      </Text>
+    </Paper>
+  );
+}
+
+function StockHistoryChart({
+  entries,
+  minDate,
+  maxDate
+}: {
+  entries: any[];
+  minDate: Date;
+  maxDate: Date;
+}) {
+  const chartSeries: any[] = useMemo(() => {
+    return [
+      {
+        name: 'quantity',
+        label: 'Quantity',
+        color: 'blue.6',
+        yAxisId: 'left'
+      },
+      {
+        name: 'value_min',
+        label: `Minimum Value`,
+        color: 'yellow.6',
+        yAxisId: 'right'
+      },
+      {
+        name: 'value_max',
+        label: `Maximum Value`,
+        color: 'teal.6',
+        yAxisId: 'right'
+      }
+    ];
+  }, [entries]);
+
+  const chartLimits: number[] = useMemo(() => {
+    return [minDate.valueOf(), maxDate.valueOf()];
+  }, [minDate, maxDate]);
+
+  const chartData: any[] = useMemo(() => {
+    return entries.map((entry) => {
+      return {
+        ...entry,
+        date: new Date(entry.date).valueOf(),
+        value_min: Number.parseFloat(entry.cost_min),
+        value_max: Number.parseFloat(entry.cost_max)
+      };
+    });
+  }, [entries]);
+
+  return (
+    <LineChart
+      h={500}
+      data={chartData}
+      dataKey='date'
+      withLegend
+      withYAxis
+      rightYAxisLabel='Stock Value'
+      yAxisLabel='Stock Quantity'
+      xAxisLabel='Date'
+      xAxisProps={{
+        domain: chartLimits,
+        scale: 'time',
+        type: 'number',
+        tickFormatter: (value: number) => {
+          return dayjs(value).format('YYYY-MM-DD');
+        }
+      }}
+      yAxisProps={{
+        allowDataOverflow: false
+      }}
+      rightYAxisProps={{
+        allowDataOverflow: false
+      }}
+      tooltipProps={{
+        content: ({ label, payload }) => (
+          <StockHistoryTooltip label={label} payload={payload} />
+        )
+      }}
+      series={chartSeries}
+    />
+  );
+}
 
 /**
  * Render a custom panel with the provided context.
@@ -62,30 +179,37 @@ function StockHistoryPanel({ context }: { context: InvenTreePluginContext }) {
 
   return (
     <>
-      <Stack gap='xs'>
-        <Group gap='xs' justify='space-apart' grow>
-          <Group gap='xs'>
-            <MonthPickerInput
-              value={startDate}
-              label={`Start Date`}
-              onChange={(value: DateValue) => {
-                if (value && value < endDate) {
-                  setStartDate(value);
-                }
-              }}
-            />
-            <MonthPickerInput
-              value={endDate}
-              label={`End Date`}
-              onChange={(value: DateValue) => {
-                if (value && value > startDate) {
-                  setEndDate(value);
-                }
-              }}
-            />
+      <Paper withBorder p='sm' m='sm'>
+        <Stack gap='xs'>
+          <Group gap='xs' justify='space-apart' grow>
+            <Group gap='xs'>
+              <MonthPickerInput
+                value={startDate}
+                label={`Start Date`}
+                onChange={(value: DateValue) => {
+                  if (value && value < endDate) {
+                    setStartDate(value);
+                  }
+                }}
+              />
+              <MonthPickerInput
+                value={endDate}
+                label={`End Date`}
+                onChange={(value: DateValue) => {
+                  if (value && value > startDate) {
+                    setEndDate(value);
+                  }
+                }}
+              />
+            </Group>
           </Group>
-        </Group>
-      </Stack>
+          <StockHistoryChart
+            minDate={startDate}
+            maxDate={endDate}
+            entries={historyQuery.data ?? []}
+          />
+        </Stack>
+      </Paper>
     </>
   );
 }
